11c0e28ec6fae9894d09846422f14f32b13a68f7
Abdul Ahad <124716424+flukeyzx@users.noreply.github.com>
2024-06-11 03:08:11 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "https://book-store-crud.vercel.app/",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 mongoose
   .connect(connectionString)
