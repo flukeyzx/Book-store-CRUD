806800b80a4f860c4a6d578b7562e14972750d1c
Abdul Ahad <124716424+flukeyzx@users.noreply.github.com>
2024-06-11 03:21:53 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://book-store-crud-frontend.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
